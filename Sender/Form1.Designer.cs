6ce61619d065126efea3827fb6811d44629799cf
bluesnooker123 <dev333perfect@gmail.com>
2023-01-19 21:10:45 +0800

    initial commit

---
+++ Sender/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_generateSignature = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_signature = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_message.Location = new System.Drawing.Point(158, 45);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(260, 26);
+            this.textBox_message.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Message";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please choose private key file";
+            // 
+            // button_open
+            // 
+            this.button_open.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_open.Location = new System.Drawing.Point(307, 107);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(111, 37);
+            this.button_open.TabIndex = 3;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            // 
+            // button_generateSignature
+            // 
+            this.button_generateSignature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_generateSignature.Location = new System.Drawing.Point(143, 164);
+            this.button_generateSignature.Name = "button_generateSignature";
+            this.button_generateSignature.Size = new System.Drawing.Size(226, 37);
+            this.button_generateSignature.TabIndex = 4;
+            this.button_generateSignature.Text = "Generate Signature";
+            this.button_generateSignature.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Signature";
+            // 
+            // textBox_signature
+            // 
+            this.textBox_signature.Enabled = false;
+            this.textBox_signature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_signature.Location = new System.Drawing.Point(158, 232);
+            this.textBox_signature.Name = "textBox_signature";
+            this.textBox_signature.Size = new System.Drawing.Size(260, 26);
+            this.textBox_signature.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_signature);
+            this.Controls.Add(this.button_generateSignature);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_message);
+            this.Name = "Form1";
+            this.Text = "Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button_generateSignature;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_signature;
+    }
+}
+
